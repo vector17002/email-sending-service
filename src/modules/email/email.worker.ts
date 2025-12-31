2f7136b090bc13af0eb2fe2746f9ea1f2fbe76a1
vector17002 <vector17002>
2025-12-31 17:46:41 +0530

    added dead letter queue and dockerised the repository

--- src/modules/email/email.worker.ts
+++ src/modules/email/email.worker.ts
@@ -2,6 +2,7 @@ import { Worker } from "bullmq";
 import { EmailProvider } from "./email.provider.js";
 import redis from "../../utils/redisClient.js";
 import { logToFile } from "../../utils/logger.js";
+import { emailDLQ } from "./email.dlq.js";
 
 const provider = new EmailProvider();
 
@@ -23,4 +24,26 @@ new Worker(
         connection: redis,
         concurrency: 5,
     }
-);
+).on("failed", async (job, error) => {
+    if (!job) {
+        logToFile("Job not found");
+        return;
+    }
+
+    logToFile(`Job ${job.id} failed:` + error);
+    const attemptsMade = job.attemptsMade;
+    const maxAttempts = job.opts.attempts ?? 1;
+
+    if (attemptsMade < maxAttempts) {
+        logToFile(`Job ${job.id} will be retried`);
+        return;
+    }
+
+    logToFile(`Job ${job.id} will be moved to DLQ`);
+    await emailDLQ.add("dead-email", {
+        originalJobId: job.id,
+        originalJobData: job.data,
+        error: error.message,
+        failedAt: new Date().toISOString(),
+    });
+})
